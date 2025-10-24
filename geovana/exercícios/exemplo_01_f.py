0e89807550e7f41ac23e76646635c12b64a424d3
Geovana386 <geoh.santino@gmail.com>
2025-10-24 20:38:54 -0300

    funcoes
    Added a script to sum two user-input numbers.

---
+++ geovana/exercícios/exemplo_01_f.py
@@ -0,0 +1,19 @@
+import os 
+os.system('cls')
+
+def somar():
+    a = int(input("Digite um número:"))
+    b = int(input("Digite um número:"))
+    total = a+b
+    print(total)
+
+
+def somar(a,b):
+    total = a+b
+    print(total)
+#-----------------------------------------------------------
+
+num_01 = int(input("Digite um número:"))
+num_02 = int(input("Digite um número:"))
+
+somar(num_01,num_02)
