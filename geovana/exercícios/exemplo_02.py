8d6a59542ece73c06200b33a2b6f0b39d0e0c50e
Geovana386 <geoh.santino@gmail.com>
2025-10-23 21:14:25 -0300

    listas
    Demonstrate list operations by removing items from 'frutas'.

---
+++ geovana/exercícios/exemplo_02.py
@@ -0,0 +1,12 @@
+import os
+os.system('cls')
+
+frutas = ["maça", "banana", "laranja" , "uva"]
+numeros = [1,2,3,4,5,6]
+mista = [1,"texto", 3.14, True]
+
+frutas.pop()
+print(frutas)
+
+frutas.pop(1)
+print(frutas)
