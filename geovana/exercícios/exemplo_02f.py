dc8782f2c385d15b642c76937bec6fa4e8426666
Geovana386 <geoh.santino@gmail.com>
2025-10-24 20:39:19 -0300

    funcoes
    Add functions to sum two numbers and calculate square root

---
+++ geovana/exercícios/exemplo_02f.py
@@ -0,0 +1,21 @@
+import os 
+os.system('cls')
+import math
+
+def somar(a,b):
+    total = a+b
+    print(total)
+
+def raizQuadrada(a):
+    raiz = math.sqrt(a)
+    print(raiz)
+#-----------------------------------------------------------
+
+num_01 = int(input("Digite um número:"))
+num_02 = int(input("Digite um número:"))
+
+#somar(num_01,num_02)
+
+raiz2 = somar(num_01,num_02)
+
+raizQuadrada = raiz2
