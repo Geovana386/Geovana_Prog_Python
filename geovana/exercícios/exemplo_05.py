fcce9865225ba47d90bb0f98b7d1f7ed5a7b4edd
Geovana386 <geoh.santino@gmail.com>
2025-10-23 21:15:49 -0300

    listas
    Add example script to demonstrate list sorting

---
+++ geovana/exercícios/exemplo_05.py
@@ -0,0 +1,14 @@
+import os
+os.system('cls')
+
+lista = [5,2,0,52,12,44,21,98,7]
+print(lista)
+
+lista.sort()
+print(lista)
+
+nomes = ["Flávia","Geovana","Ana","Paula","Joana","Beatriz","Kauana"]
+print(nomes)
+
+nomes.sort()
+print(nomes)
